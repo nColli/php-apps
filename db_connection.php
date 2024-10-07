0921721d02141db74c6ec087330d634c745c87b5
Nicolas Colli <collinicolas01@gmail.com>
2024-10-07 19:09:01 -0300

    error handling

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,15 @@
 <?php
 //PDO
 $host = 'localhost';
-$database = 'app_database';
+$database = 'app_database123';
 $user = 'user_1';
 $password = 'Password*123';
-$pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+} catch (Exception $ex) {
+    echo '<p>ERROR connecting to database</p>';
+    error_log("db_connection.php, SQL error=".$ex->getMessage());
+    return;
+}
+
 ?>
