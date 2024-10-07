3813be2aa3cf9aeea5ff0cd76fcc9319ff70a07f
Nicolas Colli <collinicolas01@gmail.com>
2024-10-07 10:58:34 -0300

    table data

--- database.php
+++ database.php
@@ -18,3 +18,25 @@ foreach ($pdo->query($sql) as $row) {
     echo '</li>';
 }
 echo '</ul>';
+
+?>
+
+<h2>Table:</h2>
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Price</th>
+    </tr>
+    <?php
+    foreach ($pdo->query($sql) as $row) {
+        echo "<tr><td>";
+        echo($row['id']);
+        echo("</td><td>");
+        echo($row['name']);
+        echo("</td><td>");
+        echo($row['price']);
+        echo("</td></tr>\n");
+    }
+    ?>
+</table>
