09bd9d8bb9ab36d755b2f2baf336d2bda1dadd49
Nicolas Colli <collinicolas01@gmail.com>
2024-10-18 13:09:26 -0300

    jquery intro

---
+++ jquery/hello.php
@@ -0,0 +1,14 @@
+<html>
+    <head></head>
+    <body>
+        <p>Here is some content</p>
+        <script type="text/javascript" src="jquery-3.7.1.min.js">
+        </script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                alert("Hello JQuery World!");
+                window.console && console.log("Hello JQuery...");
+            })
+        </script>
+    </body>
+</html>
