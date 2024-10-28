66ea5f276a2f74fb23e2219f7f5e6aec33fd381e
Nicolas Colli <collinicolas01@gmail.com>
2024-10-28 17:35:51 -0300

    save data in session

--- transport-data/index.php
+++ transport-data/index.php
@@ -1,20 +1,27 @@
 <?php
+session_start();
 require_once('config.php');
-// The URL of the API endpoint
-$url = "https://apitransporte.buenosaires.gob.ar/subtes/serviceAlerts?client_id=$client_id&client_secret=$client_secret&json=1";
 
-// Make the GET request
-$response = file_get_contents($url);
+// save json using cookies
+if (isset($_SESSION['json'])) {
+    $data = $_SESSION['json'];
+} else {
+    // The URL of the API endpoint
+    $url = "https://apitransporte.buenosaires.gob.ar/subtes/serviceAlerts?client_id=$client_id&client_secret=$client_secret&json=1";
 
-// Check if the request was successful
-if ($response === FALSE) {
-    die('Error occurred while making the GET request');
+    // Make the GET request
+    $response = file_get_contents($url);
+
+    // Check if the request was successful
+    if ($response === FALSE) {
+        die('Error occurred while making the GET request');
+    }
+    // Decode the JSON response
+    $data = json_decode($response, true);
+
+    //setcookie('json', $data, time() + 120); // '/' use to save it in entire website
+    $_SESSION['json'] = $data;
 }
-
-// Decode the JSON response
-$data = json_decode($response, true);
-
-
 ?>
 
 <pre>
@@ -26,6 +33,6 @@ $data = json_decode($response, true);
 <p>
     Array: 
     <?php
-        echo($data["entity"]["0"]["id"]);
+        print_r($data["entity"]["0"]["alert"]["informed_entity"]["0"]["route_id"]);
     ?>
 </p>
